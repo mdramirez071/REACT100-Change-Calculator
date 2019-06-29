c1d1ba2110c3066c1e4adbd5598e70eccb511c12
mdramirez071 <mdramirez071@gmail.com>
2019-06-28 17:51:01 -0700

    adds second revision of updated files

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,14 @@ class App extends Component {
       amountDue: "",
       amountReceived: "",
       calculate: "",
+      twenties: "",
+      tens: "",
+      fives: "",
+      ones: "",
+      quarters: "",
+      dimes: "",
+      nickels: "",
+      pennies: "",
       output: ""
     };
     this.onChange = this.onChange.bind(this)
@@ -17,18 +25,8 @@ class App extends Component {
     this.setState({ [e.target.name] : e.target.value })
 }  
 
-  handleClick(e) {
-    e.preventDefault(); //Stops the event at this point
-    var result = this.calculate();
-    console.log(result);
-    this.setState(
-      {output : `Your Monthly Payment Amount is: ${result}`}
-    )
-    return result;
-  }
-
   calculate(amountDue, amountReceived) {
-    
+    let array = [];
     amountDue = this.state.amountDue * 100;
     console.log(amountDue);
     amountReceived = this.state.amountReceived * 100;
@@ -42,76 +40,107 @@ class App extends Component {
    console.log(changeDue);
    var twenties = Math.floor(changeDue/2000);
    console.log(twenties);
-   changeDue = changeDue%2000;
-   console.log(changeDue);
-//store the amount of twenties into an array then return that array index
+   //store the amount of twenties into an array then return that array index
+   array.push(twenties);
  }
-
+ else { array.push(0)};
+ changeDue = changeDue%2000;
  //check if change is divisible by tens
  if(changeDue > 999)
  {
    console.log(changeDue);
    var tens = Math.floor(changeDue/1000);
    console.log(tens);
-   changeDue = changeDue%1000;
-   console.log(changDue);
    //store the amount of tens into an array then return that array index
+   array.push(tens);
  }
+ else { array.push(0)}
+ changeDue = changeDue%1000;
      //check if change is divisible by fives
      if(changeDue > 499)
      {
        console.log(changeDue);
-       var dollars = Math.floor(changeDue/500);
-       console.log(dollars);
-       changeDue = changeDue%500;
-       console.log(changeDue);
+       var fives = Math.floor(changeDue/500);
+       console.log(fives);
       //store the amount of fives into an array then return that array index
+      array.push(fives);
      }
-
+     else { array.push(0)}
+     changeDue = changeDue%500;
     //check if change is divisible by dollars
     if(changeDue > 99)
     {
       console.log(changeDue);
       var dollars = Math.floor(changeDue/100);
       console.log(dollars);
-      changeDue = changeDue%100;
-      console.log(changeDue);
       //store the amount of dollars into an array then return that array index
+      array.push(dollars);
     }
+    else { array.push(0)}
+    changeDue = changeDue%100;
+      console.log(changeDue);
     //check if change is divisible by quarters
-    if (changeDue > 24)
+    if (changeDue > 24 | changeDue > 0)
     {
       var quarters = Math.floor(changeDue/25);
-      changeDue = changeDue%25;
-      console.log(changeDue);
       //store the amount of quarters into an array then return that array index
+      array.push(quarters);
     }
+    else { array.push(0)}
+    changeDue = changeDue%25;
     //check if change is divisible by dimes
-    if (changeDue > 9 | changeDue > 0)
+    if (changeDue > 9)
     {
       //var dimes = Math.floor(changeDue-(dollars*100)-(quarters*100)/10); DONT USE! ONLY USE FOR TESTING!
       var dimes = Math.floor(changeDue/10);
-      console.log(changeDue);
-      changeDue = changeDue%10;
+      console.log(dimes);
       //store the amount of dimes into an array then return that array index
+      array.push(dimes);
     }
+    else { array.push(0)}
+    changeDue = changeDue%10;
     // check if change is divisible by nickels
-    if (changeDue > 4 | changeDue > 0)
+    if (changeDue > 4)
     {
       var nickels = Math.floor(changeDue/5);
-      changeDue = changeDue%5;
+      
       //store the amount of nickels into an array then return that array index
+      array.push(nickels);
     }
+    else { array.push(0)}
+    changeDue = changeDue%5;
     // check if change is divisible by pennies
-    if (changeDue > 0)
+    if (changeDue >= 0)
     {
       var pennies = Math.floor(changeDue/1);
       changeDue = changeDue%1;
      //store the amount of pennies into an array then return that array index
+     array.push(pennies);
     }    
-
-    return changeDue;
+    return array;
    }
+
+   handleClick(e) {
+    e.preventDefault(); //Stops the event at this point
+    var result = this.calculate();
+    console.log(result);
+    let returnChange = this.state.amountReceived - this.state.amountDue;
+    returnChange = returnChange.toFixed(2);
+    this.setState(
+      {
+      twenties: result[0],
+      tens: result[1],
+      fives: result[2],
+      ones: result[3],
+      quarters: result[4],
+      dimes: result[5],
+      nickels: result[6],
+      pennies: result[7],
+      output: `The total change due is $${returnChange}`
+    }
+    )
+    return result;
+  }
 
 
   render() {
@@ -150,17 +179,33 @@ class App extends Component {
   <div className="col-sm-6">
       <div className="card">
       <div className="card-body">
-      <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">The total change due is $98.94</div>
+      <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded"><div className="alert alert-success">
+  <strong>{this.state.output}</strong>
+</div></div>
+
+<div className="alert alert-danger">
+  <strong>Danger! Incorrect Amount of Change!</strong> You should <a href="#" className="alert-link">Read This Message</a>.
+</div>
+
       <div className="row w-100 h-50">
-    <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Twenties</div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Tens</div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Fives</div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Ones</div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Twenties
+            <p>{this.state.twenties}</p>
+            </div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Tens
+            <p>{this.state.tens}</p></div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Fives
+            <p>{this.state.fives}</p></div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Ones
+            <p>{this.state.ones}</p></div>
             <div className="row w-100 h-50"></div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Quarters</div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Dimes</div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Nickels</div>
-            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Pennies</div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Quarters
+            <p>{this.state.quarters}</p></div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Dimes
+            <p>{this.state.dimes}</p></div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Nickels
+            <p>{this.state.nickels}</p></div>
+            <div className="card mx-3 my-2 px-2 py-4 text-center col-sm bg-light rounded">Pennies
+            <p>{this.state.pennies}</p></div>
 
             </div>
 
